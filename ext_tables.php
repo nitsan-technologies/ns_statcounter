01d31ac661addce312ef53bc9a14b25c1f5ca97d
Nitsan Technologies <sanjay@nitsan.in>
2020-04-13 15:45:11 +0530

    [TODO] TYPO3 10 compatible

--- ext_tables.php
+++ ext_tables.php
@@ -1,15 +1,9 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
-call_user_func(
-    function($extKey)
-    {
 
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($extKey, 'Configuration/TypoScript', 'NsStatcounter');
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('ns_statcounter', 'Configuration/TypoScript', 'NsStatcounter');
 
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_nsstatcounter_domain_model_statcounter', 'EXT:ns_statcounter/Resources/Private/Language/locallang_csh_tx_nsstatcounter_domain_model_statcounter.xlf');
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_nsstatcounter_domain_model_statcounter');
 
-    },
-    $_EXTKEY
-);
