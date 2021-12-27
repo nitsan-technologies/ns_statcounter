95847d6e25bf0ee55ab0ba4b07d1d6cc47457b06
Maulik Lakhnotra <nitsan-gitlab@fischer-and-friends.de>
2021-12-27 19:04:25 +0530

    [TODO] Compatibility with version 11

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,14 +1,19 @@
 <?php
 defined('TYPO3_MODE') || die('Access denied.');
 
+if (version_compare(TYPO3_branch, '10.0', '>=')) {
+    $moduleClass = \Nitsan\NsStatcounter\Controller\StatcounterController::class;
+} else {
+    $moduleClass = 'Statcounter';
+}
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
     'Nitsan.NsStatcounter',
     'Nsstatcounter',
     [
-        'Statcounter' => 'list'
+        $moduleClass => 'list'
     ],
     // non-cacheable actions
     [
-        'Statcounter' => ''
+        $moduleClass => ''
     ]
 );
